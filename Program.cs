f6f29922cc54e92e9bbaec07410f592a89f6b1cb
Mateus <Mateus@DESKTOP-OVQRJ16>
2023-04-10 23:43:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Exercicios;
+
+Console.WriteLine("Entre a Largura e Altura do Retângulo: ");
+
+ var retangulo = new Retangulo();
+
+if (double.TryParse(Console.ReadLine(), out retangulo.Largura) &&
+    double.TryParse(Console.ReadLine(), out retangulo.Altura))
+{
+    double area = retangulo.Area(retangulo.Altura, retangulo.Largura);
+    double perimetro = retangulo.Perimetro(retangulo.Largura, retangulo.Altura);
+    double diagonal = retangulo.Diagonal(retangulo.Largura, retangulo.Altura);
+
+    Console.WriteLine($"Area: {area}");
+    Console.WriteLine($"Perímetro {perimetro}");
+    Console.WriteLine($"Diagonal {diagonal}");
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite apenas valores numéricos.");
+}
+
---
+++ Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area(double largura, double altura)
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro(double largura, double altura)
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal(double altura, double largura)
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+    }
+}
